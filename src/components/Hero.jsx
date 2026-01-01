1b42a8def9a387979badd10ae79ccdd9c9e3bdc6
Bishwash <drewwbishwash@gmail.com>
2026-01-02 01:37:03 +0545

    feat: initialize project with Vite, React, and Tailwind CSS
    - Add package.json with scripts and dependencies for Vite, React, and Tailwind CSS.
    - Include global CSS file for styling and font imports.
    - Create main application structure with App component and routing setup.
    - Implement Navbar and Hero components for the main layout.
    - Add SVG assets for React and Vite logos.
    - Include hero images in JPG and PNG formats.
    - Set up Vite configuration for React and Tailwind CSS integration.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import { MoveUpRight } from "lucide-react";
+
+const Hero = () => {
+  return (
+    <section
+      id="hero"
+      className="flex h-screen w-full items-center bg-dark px-32"
+    >
+      <div className="flex w-1/2 flex-col items-start gap-8 text-start">
+        <div>
+          <div className="inline-flex items-center gap-3 rounded-full bg-mid px-4 py-2">
+            <span className="size-2 rounded-full bg-white" />
+            <p className="p-notification-badge">Available for work</p>
+          </div>
+
+          <h1 className="mt-4">
+            Your trusted partner for quality home improvement
+          </h1>
+        </div>
+
+        <div>
+          <p className="p-intro mb-6">
+            Refit delivers expert home improvements, creating beautiful and
+            functional spaces with quality craftsmanship.
+          </p>
+
+          <button
+            type="button"
+            className="flex h-16 items-center justify-between gap-6 rounded-full bg-mid px-4"
+          >
+            <span className="ml-2 p-button-text">Work with us</span>
+
+            <span className="rounded-full bg-white p-2">
+              <MoveUpRight size={20} className="text-black" />
+            </span>
+          </button>
+        </div>
+      </div>
+
+      <div className="flex w-1/2">
+        <div className="absolute top-0 right-0 h-screen bg-linear-to-l from-black to-transparent" />
+        <img
+          src="images/hero-img.jpg"
+          alt="Hero Image"
+          className="h-screen w-full py-8 object-cover rounded-3xl"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+const Navbar = () => {
+  const Navlinks = [
+    { title: "About", link: "#about" },
+    { title: "Services", link: "#services" },
+    { title: "Our Works", link: "#ourworks" },
+    { title: "FAQs", link: "#faqs" },
+    { title: "Contact", link: "#contact" },
+  ];
+  return (
+    <nav className="fixed z-50 w-full bg-transparent px-32">
+      <div className="flex md:flex-row flex-col md:justify-between items-center gap-5 py-8 lg:px-0 mx-auto">
+        <h3 className="flex-1/2">
+          <a href="#home" className="text-3xl">Refit</a>
+        </h3>
+        <ul className="flex md:flex-row justify-center items-center gap-12 flex-1/2">
+          {Navlinks.map((navlink, index) => (
+            <a href={navlink.link} key={index} className="p-menu-item no-underline">
+              {navlink.title}
+            </a>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
